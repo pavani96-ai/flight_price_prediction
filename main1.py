b4191a61f6b406ecc9300197c426b507205ad886
pavani <pavanikusuma2020@gmail.com>
2026-06-18 10:51:22 +0000

    trail 5

--- main1.py
+++ main1.py
@@ -2,6 +2,16 @@ from src.flight_price_prediction.exception.exception import CustomException
 from src.flight_price_prediction.logging.logger import logging
 from src.flight_price_prediction.pipeline.train_pipeline import TrainPipeline
 import sys
+import os, dagshub
+
+user = os.getenv("DAGSHUB_USER")
+token = os.getenv("DAGSHUB_TOKEN")
+
+if not user or not token:
+    raise RuntimeError("DAGSHUB_USER or DAGSHUB_TOKEN not found in environment")
+
+dagshub.auth.add_app_token(token)
+dagshub.init(repo_owner=user, repo_name="flight_price_prediction", mlflow=True)
 
 STAGE_NAME = " TRAINING PIPELINE"
 
